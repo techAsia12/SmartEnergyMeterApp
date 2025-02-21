2d1d2f696f2ec8201c76e412797d672802484642
Priyesh Chiplunkar <techasiamechatronics@gmail.com>
2025-02-21 16:43:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smartenergymeter/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.smartenergymeter;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+        webView.setScrollbarFadingEnabled(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_ALWAYS);
+
+        webView.loadUrl("https://smartenergymeter.techasiamechatronics.com/");
+    }
+}
